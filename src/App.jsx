2a86c35381c8321283b2b4b9922c0660d003879b
Alexis Vega <alexisvegamontoya@gmail.com>
2026-06-29 14:48:07 -0600

    feat: módulos Evaluación de Riesgos y Plan de Acción

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ import AuditLog from './pages/admin/AuditLog'
 import ConsultaPEP from './pages/ConsultaPEP'
 import DebilidaDiligencia from './pages/DebilidaDiligencia'
 import ModuloIA from './pages/ModuloIA'
+import EvaluacionRiesgos from './pages/EvaluacionRiesgos'
+import PlanAccion from './pages/PlanAccion'
 
 function Layout({ children }) {
   return (
@@ -143,6 +145,16 @@ function AppRoutes() {
       <Route path="/perfil" element={
         <PrivateRoute>
           <Layout><Perfil /></Layout>
+        </PrivateRoute>
+      } />
+      <Route path="/evaluacion" element={
+        <PrivateRoute>
+          <Layout><EvaluacionRiesgos /></Layout>
+        </PrivateRoute>
+      } />
+      <Route path="/plan-accion" element={
+        <PrivateRoute>
+          <Layout><PlanAccion /></Layout>
         </PrivateRoute>
       } />
       <Route path="*" element={<Navigate to="/" replace />} />
